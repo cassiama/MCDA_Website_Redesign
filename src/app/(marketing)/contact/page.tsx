edb7b0de94efd77bfb4e985b53b6f4115007befa
Chancelor Assiamah <chanceyassiamah@gmail.com>
2025-08-18 02:00:35 +0000

    fixed the broken styling for the contact page (finally).

--- src/app/(marketing)/contact/page.tsx
+++ src/app/(marketing)/contact/page.tsx
@@ -40,13 +40,21 @@ type SocialCardProps = {
 const SocialCard = ({ href, label, icon }: SocialCardProps) => {
     return (
         <div className="flex flex-col items-center gap-3">
-            <Link href={href} className="focus:outline-none focus-visible:ring-4 ring-primary/30 rounded-full">
+            <Link
+                href={href}
+                className="focus:outline-none focus-visible:ring-4 ring-primary rounded-full hover:scale-[1.03] focus-visible:scale-[1.03]"
+            >
                 <span className="sr-only">{`Follow us on ${label}`}</span>
-                <div className="size-20 rounded-full bg-teal-600/80 shadow-elev-300 grid place-items-center">
-                    <Icon name={icon} className="size-10 text-white" />
+                <div
+                    className="size-20 rounded-full bg-card-border shadow-elev-300 grid place-items-center"
+                >
+                    <Icon
+                        name={icon}
+                        className="size-14 text-white"
+                    />
                 </div>
             </Link>
-            <p className="text-body-md text-center">Follow Us on {label}</p>
+            <p className="w-24 text-body-md text-center">Follow Us on {label}</p>
         </div>
     );
 }
@@ -56,45 +64,50 @@ const ContactPage = (): JSX.Element => {
     return (
         // 2-col grid on md+ breakpoints, 1-col on mobile
         <div>
-            {/* contact section (left sidebar) */}
             <div
-                className="max-w-screen-2xl grid grid-cols-1 md:grid-cols-12 gap-6"
+                className="max-w-screen-2xl grid grid-cols-1 lg:grid-cols-12"
             >
-
+                {/* contact section (left sidebar on desktop) */}   
                 <aside
-                    className="md:col-span-4 flex flex-col gap-6"
+                    className="lg:col-span-4 flex flex-col gap-6 py-3"
                 >
                     {/* page header */}
                     <h1
-                        className="heading-1 px-4 md:px-6 pt-3"
+                        className="heading-1 mx-auto md:mx-0 px-4 md:px-6 pt-3"
                     >
                         Contact Us
                     </h1>
-                    <p className="section text-body-md text-muted-foreground px-4 md:px-6">
+                    <p
+                        className="section text-body-md text-muted-foreground px-6 py-3"
+                    >
                         Let us know how we can help make a difference in your community. We are stronger together!
                     </p>
 
                     {/* contact info cards */}
                     <ul
-                        className="flex flex-col gap-4 px-4 md:px-6"
+                        className="flex flex-col gap-4 px-4 md:px-6 mx-auto"
                     >
                         {CONTACTS.map(({ icon, title, label }) => (
                             <li
                                 key={`${icon}-${label}`}
-                                className="rounded-xl bg-card shadow-elev-300 p-4 flex items-center gap-4"
+                                className="w-full max-w-3xs lg:max-w-xs rounded-xl bg-card shadow-elev-300 p-4 flex flex-col lg:flex-row items-center gap-4"
                             >
+                                {/* icon */}
                                 <div
                                     className="shrink-0 rounded-lg bg-primary p-3"
                                 >
                                     <Icon
                                         key={label.toLowerCase()}
-                                        name={icon} className="size-8 text-white"
+                                        name={icon}
+                                        className="size-6 md:size-8 text-white"
                                     />
                                 </div>
+
+                                {/* card content */}
                                 <div
                                     className="min-w-0"
                                 >
-                                    {title && <p className="text-body-md text-disabled-text flex-1 break-all">{title}</p>}
+                                    {title && <p className="text-body-md text-disabled-text">{title}</p>}
                                     <p className="text-body-md text-text break-words">{label}</p>
                                 </div>
                             </li>
@@ -103,7 +116,7 @@ const ContactPage = (): JSX.Element => {
 
                     {/* map */}
                     <div
-                        className="section overflow-hidden shadow-elev-300 px-4 md:px-6 py-3"
+                        className="section overflow-hidden px-4 md:px-6 py-3"
                     >
                         <iframe
                             title="MC Diversity Alliance location"
@@ -119,13 +132,20 @@ const ContactPage = (): JSX.Element => {
                     </div>
 
                     {/* social media */}
-                    <section aria-labelledby="follow-us">
-                        <h2 id="follow-us" className="heading-2 p-4">Follow Us</h2>
-                        <p
-                            className="text-body-md text-text px-4"
-                        >
-                            Stay up-to-date on all the latest news and happenings at Monmouth County Diversity Alliance Inc. by following us on social media. We're active on Facebook and Instagram.
-                        </p>
+                    <h2
+                        id="follow-us"
+                        className="heading-2 mx-auto md:mx-0 px-5"
+                    >
+                        Follow Us
+                    </h2>
+                    <p
+                        className="text-body-md text-text px-5"
+                    >
+                        Stay up-to-date on all the latest news and happenings at Monmouth County Diversity Alliance Inc. by following us on social media. We're active on Facebook and Instagram.
+                    </p>
+                    <div
+                        className="grid md:grid-cols-2 md:py-2 justify-items-center gap-3"
+                    >
                         {CONTACT_ICONS.map(({ label, href, icon }) => (
                             <SocialCard
                                 key={label.toLowerCase()}
@@ -134,17 +154,23 @@ const ContactPage = (): JSX.Element => {
                                 icon={icon}
                             />
                         ))}
-                    </section>
+                    </div>
                 </aside>
-                {/* form section */}
-                <main className="md:col-span-8 section border-l-2">
+                
+                {/* form section (right sidebar on desktop) */}
+                <main 
+                    className="lg:col-span-8 section lg:border-l-2 lg:rounded-tl-lg border-b-0 shadow-none"
+                >
                     <section 
-                        className="shadow-elev-300 p-5 md:p-6"
+                        className="p-5 lg:p-6"
                     >
                         <form
-                            className="grid grid-cols-1 gap-4 md:grid-cols-2"
+                            className="flex flex-col gap-3"
                         >
-                            <div className="flex flex-col gap-1.5">
+                            {/* fields */}
+                            <div
+                                className="flex flex-col gap-1"
+                            >
                                 <label
                                     htmlFor="name-input"
                                     className="text-body font-medium text-foreground"
@@ -158,7 +184,9 @@ const ContactPage = (): JSX.Element => {
                                     className="field"
                                 />
                             </div>
-                            <div>
+                            <div
+                                className="flex flex-col gap-1"
+                            >
                                 <label
                                     htmlFor="email-input"
                                     className="text-body font-medium text-foreground"
@@ -172,7 +200,9 @@ const ContactPage = (): JSX.Element => {
                                     className="field"
                                 />
                             </div>
-                            <div>
+                            <div
+                                className="flex flex-col gap-1"
+                            >
                                 <label
                                     htmlFor="subject-input"
                                     className="text-body font-medium text-foreground"
@@ -186,7 +216,9 @@ const ContactPage = (): JSX.Element => {
                                     className="field"
                                 />
                             </div>
-                            <div>
+                            <div
+                                className="flex flex-col gap-1"
+                            >
                                 <label
                                     htmlFor="msg-input"
                                     className="text-body font-medium text-foreground"
@@ -197,11 +229,13 @@ const ContactPage = (): JSX.Element => {
                                 <textarea 
                                     id="msg-input"
                                     placeholder="Type your message..."
-                                    className="textarea"
+                                    rows={4}
+                                    className="textarea min-h-10 max-h-25"
                                 />
                             </div>
 
-                            <div className="md:col-span-2">
+                            {/* submit button */}
+                            <div className="inline-flex justify-end mx-auto lg:mx-0">
                                 <button
                                     type="submit"
                                     className="btn btn-primary"
--- src/components/core/contact-icon.tsx
+++ src/components/core/contact-icon.tsx
@@ -18,15 +18,11 @@ const ContactIcon = ({ href, label, icon }: ContactIconProps): JSX.Element => {
         <Link
             href={href}
             aria-label={label}
-            className="
-            flex items-center justify-center
-            size-24 md:size-28 p-3 md:p-6 rounded-full
-            bg-card-border shadow-elev-300 ring-4 ring-white
-            transition-transform hover:scale-[1.03] focus-visible:scale-[1.03]"
+            className="flex items-center justify-center size-24 md:size-26 p-3 md:p-6 rounded-full bg-card-border shadow-elev-300 ring-4 ring-white transition-transform hover:scale-[1.03] focus-visible:scale-[1.03]"
         >
             <Icon 
                 name={icon}
-                className="block size-[85%] overflow-visible"
+                className="block size-[95%] md:size-[125%] overflow-visible"
             />
             <span className="sr-only">{label}</span>
         </Link>
